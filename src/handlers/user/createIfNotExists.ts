040e98af9391c3971b687124f2dc5ce8793671b9
Mehdi Meddour <contact@mehdi.cafe>
2023-04-25 10:53:43 +0200

    feat: Add password hashing using pbkdf2 (#49)

--- src/handlers/user/createIfNotExists.ts
+++ src/handlers/user/createIfNotExists.ts
@@ -1,5 +1,6 @@
 import { createIfNotExists } from '../../adapters/db/prisma/UserPersistance'
+import { hash } from '../../adapters/encryption/pbkdf2/PasswordHasher'
 import handlerWrapper from '../../adapters/http/middy/handlerWrapper'
 import { handler, validator } from '../../useCases/user/createIfNotExists'
 
-export default handlerWrapper(validator, handler({ createIfNotExists }))
+export default handlerWrapper(validator, handler({ createIfNotExists, hash }))
